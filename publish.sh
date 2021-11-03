2b3f22187f10aa2f718db67120fd2651a38ecc49
Uma Zalakain <ping@umazalakain.info>
2021-11-03 03:47:33 +0000

    Add link to talk

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ git branch -f gh-pages main
 git checkout gh-pages
 agda --html --html-dir=site/agda Everything.agda
 (cd Talk; pdflatex talk.tex; cp talk.pdf ../site/talk.pdf)
-(cd site; cabal new-run site build)
+(cd site; cabal new-run site clean; cabal new-run site build)
 git add -f site/agda docs
 git commit -m "Update HTML pages"
 git push -f origin gh-pages
